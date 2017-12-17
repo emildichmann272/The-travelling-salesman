ee64261c50464c1e149fa8c12fe440e85a6513de
Emil Dichmann <emil@dichmann.info>
2017-12-17 20:34:27 +0100

    Added Fitness calculations and Selection algorithms

--- main.py
+++ main.py
@@ -1,30 +1,75 @@
-import turtle, time, math, random, sys
+import turtle, time, math, random, sys, copy
 
-if (len(sys.argv)<4):
+if (len(sys.argv)<6):
 	print("not enough arguments")
-	print("Cities, Population, Mutation")
+	print("Cities, Population, Selection, Mutation, Generation")
 	sys.exit()
 
-argCities = sys.argv[1]
-argPopulation = sys.argv[2]
-argMutation = sys.argv[3]
-
-global Cities, Population
+global Cities, Population, Generation, Selection, argGeneration
+	
+argCities = int(sys.argv[1])
+argPopulation = int(sys.argv[2])
+Selection = int(sys.argv[3])
+argMutation = int(sys.argv[4])
+argGeneration = int(sys.argv[5])
 Cities = []
-#City generation
-for x in range(int(argCities)):
-	city = [random.randint(0,200), random.randint(0,200)]
-	Cities.append(city)
-	# print("city "+ str(x+1) + "	[" + str(city[0]) + ", " + str(city[1])+"]")
-
 Population = []
-for x in range(int(argPopulation)):
-	solution = []
-	citysol = Cities[:]
-	for x in range(len(Cities)):
-		integer = random.randint(0,len(citysol)-1)
-		solution.append(citysol[integer])
-		citysol.pop(integer)
-	# print(solution)
-	Population.append(solution)
-# print(Population)
+Generation = 0
+
+#City generation
+def initialSetup():
+	for x in range(argCities):
+		city = [random.randint(0,200), random.randint(0,200)]
+		Cities.append(city)
+		print("city "+ str(x+1) + "	[" + str(city[0]) + ", " + str(city[1])+"]")
+
+	for x in range(argPopulation):
+		solution = []
+		citysol = Cities[:]
+		for x in range(len(Cities)):
+			integer = random.randint(0,len(citysol)-1)
+			solution.append(citysol[integer])
+			citysol.pop(integer)
+		print(solution)
+		Population.append(Solution(solution))	
+
+def calculateFitness():
+	for x in range(len(Population)):	
+		Population[x].calcfitness()
+	Population.sort(key=lambda X: X.fitness, reverse=False)
+	for x in range(len(Population)):
+		print(Population[x].fitness)
+
+		
+def select():
+	Selected = []
+	for x in range(len(Population)):
+		Chosen = False
+		k = 0
+		while (Chosen == False):
+			k += 1
+			if (random.randint(1,100) <= Selection):
+				Chosen = True
+			if (k == len(Population)):
+				Chosen = True
+		print(k)
+		Selected.append(copy.deepcopy(Population[k-1]))
+	
+	
+def newGeneration():
+	calculateFitness()
+	select()
+		
+		
+class Solution():
+	def __init__(self, solution):
+		self.solution = solution
+		self.fitness = 0
+	
+	def calcfitness(self):
+		self.fitness = 0
+		for x in range(len(self.solution)-1):
+			self.fitness += math.sqrt(math.pow(self.solution[x+1][0]-self.solution[x][0],2)+math.pow(self.solution[x+1][1]-self.solution[x][1],2));
+
+initialSetup()
+newGeneration()
